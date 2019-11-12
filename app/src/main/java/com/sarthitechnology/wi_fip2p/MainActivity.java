02332fb793fe358ff00c647cce63b57d4c6d4b50
lochana chathura <chathux.lc@gmail.com>
2019-11-12 14:35:35 +0530

    initial work

--- app/src/main/java/com/sarthitechnology/wi_fip2p/MainActivity.java
+++ app/src/main/java/com/sarthitechnology/wi_fip2p/MainActivity.java
@@ -60,8 +60,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-//        StrictMode.setThreadPolicy(policy);
         setContentView(R.layout.activity_main);
         initialWork();
         exqListener();
